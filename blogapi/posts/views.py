f282736b08a63e29cb77271f359b76ab1e7761ae
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-03-27 20:32:51 +0900

    Add Chapter 5, 6.

---
+++ blogapi/posts/views.py
@@ -0,0 +1,16 @@
+# Create your views here.
+from rest_framework import generics
+
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
